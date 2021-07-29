e8dbc94c906b5687a5e8447b0f64d4b313cd66f6
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 10:47:56 +0000

    db added change to test.sh (python env fix)

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,5 +1,6 @@
 #! bin/bash
 
+python3 -m venv venv
 source venv/bin/activate
 python3 -m pytest --cov=application --cov-report=term-missing
 python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
