7f150d9001a863191655d20a825ac9a1e0854350
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 13:23:43 +0000

    test.sh commented out again

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,8 @@ source venv/bin/activate
 # python3 -m venv venv
 # pip3 install pytest pytest-cov flask_testing
 
-python3 -m pytest --cov=application --cov-report=term-missing
-python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
+# python3 -m pytest --cov=application --cov-report=term-missing
+# python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
 
 
 
