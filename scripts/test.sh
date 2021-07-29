3c2105fe9f264fc4aa90e12a205dc90473732d29
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 13:53:20 +0000

    changed test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #! bin/bash
 
+python3 -m venv venv
 source venv/bin/activate
 
-# python3 -m venv venv
-# pip3 install pytest pytest-cov flask_testing
+pip3 install pytest pytest-cov flask_testing
 
 python3 -m pytest --cov=application --cov-report=term-missing
 python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
