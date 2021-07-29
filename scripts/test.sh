99762423fb0de0ff027f2310ecf237956db731d9
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 14:00:22 +0000

    removed lines from test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,7 @@
 #! bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
 
-# pip3 install pytest pytest-cov flask_testing
+source venv/bin/activate
 
 python3 -m pytest --cov=application --cov-report=term-missing
 python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
