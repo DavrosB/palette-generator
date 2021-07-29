c958472aa71ef66745ca78c8525a20d3d4844f98
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 14:11:27 +0000

    added JUnit and Cobertura

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,5 +6,11 @@ source venv/bin/activate
 python3 -m pytest --cov=application --cov-report=term-missing
 python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
 
+junit allowEmptyResults: true, skipMarkingBuildUnstable: true, testResults: 'junit/test-results.xml'
+
+cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: 'coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+
+
+
 
 
