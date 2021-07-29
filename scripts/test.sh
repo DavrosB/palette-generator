6f869bf6c26457909ad292f51d66ed8d0e3df9f4
Ubuntu <ubuntu@ip-172-31-13-226.eu-west-2.compute.internal>
2021-07-29 14:35:08 +0000

    added junit and covertura test stats

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,19 @@ source venv/bin/activate
 python3 -m pytest --cov=application --cov-report=term-missing
 python3 -m pytest --cov=application --junitxml=junit.xml --cov-report=xml --cov-report=term-missing
 
+pipeline {
+    agent any
 
+    # stages {
+    #     ...
+    # }
+    post {
+        always {
+            junit '**/*.xml'
+            step([$class: 'CoberturaPublisher', autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/coverage.xml', failUnhealthy: false, failUnstable: false, maxNumberOfBuilds: 0, onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false])
+        }
+    }
+}
 
 
 
